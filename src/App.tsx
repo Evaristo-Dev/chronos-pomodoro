93634dddd191dc864624177e93ebd13f43a669d6
andre <andreevaristotavares@outlook.com>
2026-04-15 21:32:51 -0300

    css global e theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import './App.css'
-
 function App() {
-  return (
-    <>
-      <h1>Chronos Pomodoro</h1>
+    return <>
+        <h1 className="h1">Olá, Chronos Pomodoro!</h1>
     </>
-  )
 }
 
 export default App
